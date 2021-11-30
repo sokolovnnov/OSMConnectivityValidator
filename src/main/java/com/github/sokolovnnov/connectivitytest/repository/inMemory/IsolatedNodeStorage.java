a88bf9901f9afb7aaa3347afe1fe9a39fe9e5b63
ILYA <sokolovnnov@gmail.com>
2021-11-30 19:31:48 +0300

    jdk 8

--- src/main/java/com/github/sokolovnnov/connectivitytest/repository/inMemory/IsolatedNodeStorage.java
+++ src/main/java/com/github/sokolovnnov/connectivitytest/repository/inMemory/IsolatedNodeStorage.java
@@ -4,15 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.sokolovnnov.connectivitytest.model.SimpleNode;
 import org.springframework.stereotype.Component;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class IsolatedNodeStorage implements Externalizable {
+public class IsolatedNodeStorage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     public List<SimpleNode> simpleNodes = new ArrayList<>();
 
@@ -20,24 +19,24 @@ public class IsolatedNodeStorage implements Externalizable {
     public IsolatedNodeStorage() {
     }
 
-    @Override
-    public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeInt(simpleNodes.size());
-        for (SimpleNode simpleNode : simpleNodes) {
-            out.writeObject(simpleNode);
-        }
-    }
-
-    @Override
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        List<SimpleNode> simpleNodes = new ArrayList<>();
-        int size = in.readInt();
-        for (int i = 0; i < size; i++) {
-            SimpleNode simpleNode = (SimpleNode) in.readObject();
-            simpleNodes.add(simpleNode);
-        }
-        this.simpleNodes = simpleNodes;
-    }
+//    @Override
+//    public void writeExternal(ObjectOutput out) throws IOException {
+//        out.writeInt(simpleNodes.size());
+//        for (SimpleNode simpleNode : simpleNodes) {
+//            out.writeObject(simpleNode);
+//        }
+//    }
+//
+//    @Override
+//    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+//        List<SimpleNode> simpleNodes = new ArrayList<>();
+//        int size = in.readInt();
+//        for (int i = 0; i < size; i++) {
+//            SimpleNode simpleNode = (SimpleNode) in.readObject();
+//            simpleNodes.add(simpleNode);
+//        }
+//        this.simpleNodes = simpleNodes;
+//    }
 
 
 }
